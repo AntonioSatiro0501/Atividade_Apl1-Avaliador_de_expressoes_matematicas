2fc21a45072de19a5502c59d9a9f6e6767a55b8d
ANTONIO COSTA SATIRO DE SOUZA <10723636@mackenzista.com.br>
2025-09-16 13:29:20 +0000

    Adição comentários em FilaSequencialGnerica

--- FilaSequencialGenerica.java
+++ FilaSequencialGenerica.java
@@ -1,21 +1,26 @@
 public class FilaSequencialGenerica <T> {
 
+    // atributos
     private static int Tamanho = 100; 
     private int inicio = 0, fim = 0;
     private T e[ ];
 
+    // inicializador
     public FilaSequencialGenerica(){
         e = (T[]) new Object[Tamanho];
     }
 
+    // verifica se a fila esta vazia
     public boolean qIsEmpty() {
 		return (this.inicio == this.fim);	
 	}
 
+    // verifica se a fila esta cheia
     public boolean qIsFull() {
     	return (this.fim == Tamanho);
     }
 
+    // adiciona valor a fila caso nao esteja cheia
     public void enqueue(T e) throws Exception {
 		if (! qIsFull( )){
 			    this.e[this.fim++] = e;
@@ -24,6 +29,7 @@ public class FilaSequencialGenerica <T> {
 			throw new Exception("Oveflow - Estouro de Fila");	
 	}
     
+    // remove primeiro valor da fila
     public T dequeue() throws Exception {
     	  if (! qIsEmpty( )){
     		  return this.e[ this.inicio++];
@@ -32,6 +38,7 @@ public class FilaSequencialGenerica <T> {
     	  }
     }
 
+    // retorna valor na primeira posicao da fila
     public T front() throws Exception {
 		if (! qIsEmpty())
 			return e[inicio];
@@ -40,6 +47,7 @@ public class FilaSequencialGenerica <T> {
 		}			
 	}
 
+    // retorna ultimo valor da fila
     public T rear() throws Exception {
 		if (! qIsEmpty()){
 			  int pfinal = this.fim - 1;
@@ -49,6 +57,7 @@ public class FilaSequencialGenerica <T> {
 		}			
 	}
 
+    // retorna total de elementos da fila
     public	int totalElementos(){
 		if (!qIsEmpty()) return this.fim - this.inicio;
 		else return 0;
