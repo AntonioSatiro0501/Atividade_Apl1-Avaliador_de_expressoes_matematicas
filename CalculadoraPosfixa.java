7233f872224818ccc0700de90edadd5937e48f58
ANTONIO COSTA SATIRO DE SOUZA <10723636@mackenzista.com.br>
2025-09-16 13:45:25 +0000

    Inicio classe CalculadoraPosfixa (atributos e contrutor)

---
+++ CalculadoraPosfixa.java
@@ -0,0 +1,13 @@
+public class CalculadoraPosfixa {
+
+// atributos
+FilaSequencialGenerica fila;
+PilhaGenerica pilha = new PilhaGenerica<String>();
+
+// construtor
+public CalculadoraPosfixa(FilaSequencialGenerica fila){
+    this.fila = fila;
+} 
+
+
+}
