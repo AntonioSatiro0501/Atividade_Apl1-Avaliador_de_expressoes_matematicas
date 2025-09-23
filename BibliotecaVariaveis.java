d28d17ed35a621c3136c874182ad8e997a5d4664
ANTONIO COSTA SATIRO DE SOUZA <10723636@mackenzista.com.br>
2025-09-23 17:07:30 -0300

    Adição de BibliotecaVariaveis

---
+++ BibliotecaVariaveis.java
@@ -0,0 +1,56 @@
+public class BibliotecaVariaveis{
+
+    // Atributos
+    private boolean[]definida;
+    private double[]valor;
+
+    // Construtor: Define os Vetores de Atributos com Tamanho 26 (A a Z)
+    public BibliotecaVariaveis(){
+        inicializada = new boolean[26]; 
+        valor = new double[26];
+    }
+
+    // Define o Valor da Variável:
+    public void setValor(char letra, double val){
+        int indice = Character.toUpperCase(letra) - 'A'; // Encontra o Índice do Caractere no Alfabeto
+        inicializada[indice] = true; // Variável Inicializada
+        valor[indice] = val; // Variável tem Valor val
+    }
+
+    // Reinicia o Valor de Todas as Variáveis
+    public void reset(){
+        // Percorre Todo o Vetor de Alfabeto
+        for(int i = 0; i < 26; i++){
+            inicializada[i] = false; // Reinicia as Variáveis
+            valor[i] = 0; // Define o Valor Padrão para 0
+        }
+
+    }
+    // Verifica se a Variável foi Inicializada
+    public boolean isInicializada(char letra){
+        int indice = Character.toUpperCase() - 'A'; // Obtém o Índice da Letra
+        return inicializada[indice];
+    }
+
+    // Obtém o Valor Definido da Variável
+    public double getValor(char letra){
+        int indice = Character.toUpperCase() - 'A' // Obtém o Índice da Letra
+        return valor[indice];
+    }
+
+    // Mostra Todas as Variáveis Definidas Para o Usuário
+    public void mostraVariaveis(){
+        boolean nenhuma = true; // Inicialmente, Supõe-se que Nenhuma Variável foi Definida
+
+        for(int i = 0; i < 26; i++){
+            if(inicializada[i]){
+                nenhuma = false; // Há Pelo menos uma Variável que foi Inicializada
+            }
+            System.out.printf("%c = %f\n", (((char)'A'+ i),valor[i]));
+        }
+
+        if(nenhuma){ // Se Nenhuma Variável foi Inicializada
+            System.out.println("Nenhuma Variável Definida"); // Exibe a Mensagem ao Usuário
+        }
+    }
+}
